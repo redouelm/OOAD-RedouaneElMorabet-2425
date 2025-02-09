dc7748ef47ebb5d71bb54da787ffdb6ac77276d7
redouelm <redouane.elmorabet@student.odisee.be>
2025-02-09 18:53:28 +0100

    TaakLes01

---
+++ SlnLes01HerhalingAanvulling/ConsoleComplexiteit/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace ConsoleComplexiteit
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string input;
+
+            do
+            {
+                Console.Write("Geef een woord (enter om  te stoppen): ");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    // karakters tellen + resultaat
+                    Console.WriteLine($"aantal karakters: {input.Length}");
+
+                    // lettergrepen tellen + resultaat
+                    int lettergrepen = AantalLettergrepen(input);
+                    Console.WriteLine($"aantal lettergrepen: {lettergrepen}");
+
+                    // complexitet berekenen + resultaat
+                    double complexi = Complexiteit(input);
+                    Console.WriteLine($"Complexiteit: {Math.Round(complexi, 1)}");
+                    Console.WriteLine(" ");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+            }
+            while (!string.IsNullOrEmpty(input));
+
+            Console.WriteLine("Bedankt en tot ziens!");
+        }
+
+        static bool IsKlinker(char a)
+        {
+            return "aeiouAEIOU".Contains(a);
+        }
+
+        static int AantalLettergrepen(string input)
+        {
+            int lettergrepen = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsKlinker(input[i]))
+                {
+                    if (i == 0 || !IsKlinker(input[i - 1]))
+                    {
+                        lettergrepen++;
+                    }
+                }
+            }
+            return lettergrepen;
+
+        }
+        static double Complexiteit(string input)
+        {
+            double totaal = input.Length / 3.0 + AantalLettergrepen(input);
+
+            for (int i = 0; i < input.Length;)
+            {
+                if ("xyq".Contains(input[i]))
+                {
+                    totaal++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return totaal;
+        }
+    }
+}
---
+++ SlnLes01HerhalingAanvulling/WpfComplexiteit/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfComplexiteit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string input = txtInput.Text;
+
+            int aantalKarakters = input.Length;
+            int lettergrepen = AantalLettergrepen(input);
+            double complexi = Complexiteit(input);
+            txtBkOutput.Text = $@"Aantal karakters: {aantalKarakters}
+Aantal lettergrepen: {lettergrepen}
+Complexiteit: {Math.Round(complexi, 1)}";
+        }
+
+        static bool IsKlinker(char a)
+        {
+            return "aeiouAEIOU".Contains(a);
+        }
+        static int AantalLettergrepen(string input)
+        {
+            int lettergrepen = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsKlinker(input[i]))
+                {
+                    if (i == 0 || !IsKlinker(input[i - 1]))
+                    {
+                        lettergrepen++;
+                    }
+                }
+            }
+            return lettergrepen;
+        }
+        static double Complexiteit(string input)
+        {
+            double totaal = input.Length / 3.0 + AantalLettergrepen(input);
+
+            for (int i = 0; i < input.Length;)
+            {
+                if ("xyq".Contains(input[i]))
+                {
+                    totaal++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return totaal;
+        }
+    }
+}
